481e546135bcd688f993d3602e5ae556fb7d3e56
Nabilla Putri Venia Bangko <nabillavenia01@gmail.com>
2026-06-04 09:11:36 +0700

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -1,37 +1,28 @@
-"""
-Package: service
-Package for the application models and service routes
-This module creates and configures the Flask app and sets up the logging
-and SQL database
-"""
-import sys
+import os
+import logging
 from flask import Flask
-from service import config
-from service.common import log_handlers
+from flask_cors import CORS
+from flask_talisman import Talisman
 
 # Create Flask application
 app = Flask(__name__)
-app.config.from_object(config)
 
-# Import the routes After the Flask app is created
-# pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
-from service import routes, models  # noqa: F401 E402
+# Enable Cross-Origin Resource Sharing (CORS)
+CORS(app)
 
-# pylint: disable=wrong-import-position
-from service.common import error_handlers, cli_commands  # noqa: F401 E402
+# Enable Security Headers using Flask-Talisman
+talisman = Talisman(
+    app,
+    content_security_policy={
+        'default-src': '\'self\'',
+        'object-src': '\'none\'',
+    },
+    force_https=False # Set to True in production
+)
+
+# Import the routes and models after app is created
+from service import routes, models
+from service.common import error_handlers
 
 # Set up logging for production
-log_handlers.init_logging(app, "gunicorn.error")
-
-app.logger.info(70 * "*")
-app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
-app.logger.info(70 * "*")
-
-try:
-    models.init_db(app)  # make our database tables
-except Exception as error:  # pylint: disable=broad-except
-    app.logger.critical("%s: Cannot continue", error)
-    # gunicorn requires exit code 4 to stop spawning workers when they die
-    sys.exit(4)
-
-app.logger.info("Service initialized!")
+print(f"Setting up logging hierarchy for {__name__}...")
